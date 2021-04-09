45e08ae39c98b92995b37cf982b412217ddadd45
JangIBin <wkddlqls321@gmail.com>
2021-04-09 14:09:27 +0900

    Heading.js 생성

--- app/App.js
+++ app/App.js
@@ -2,6 +2,14 @@ import React from 'react;
 import { View, ScrollView, StyleSheet } from 'react-native'
 
 class App extends Components {
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All'
+    }
+  }
   render() {
     return (
       <View style={style.container}>
