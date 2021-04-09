ab07a021ac576e44089f8724879ef56e5799fdc4
JangIBin <wkddlqls321@gmail.com>
2021-04-09 13:40:42 +0900

    App.js 수정

---
+++ app/App.js
@@ -0,0 +1,25 @@
+import React from 'react;
+import { View, ScrollView, StyleSheet } from 'react-native'
+
+class App extends Components {
+  render() {
+    return (
+      <View style={style.container}>
+        <ScrollView keyboardShouldPersistTaps='always' style={style.content}></ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#f5f5f5'
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
+
+export default App;
